6c46587145f93f2a49c87c32d0ce892188b8fbd2
王智成 <Srar@MacBook-Pro.local>
2016-07-04 18:56:41 +0800

    update

--- src/js/components/WindowControlClose.js
+++ src/js/components/WindowControlClose.js
@@ -1,14 +1,21 @@
 import React, { Component, PropTypes } from 'react';
+const remote = require('electron').remote;
 
 class GreenButton extends Component {
     constructor(props) {
         super(props);
+        console.log(remote);
+    }
+
+    __onClickClose(){
+        var window = remote.getCurrentWindow();
+        window.close();
     }
 
     render() {
         return (
             <div className="window-controls">
-                <div className="close"></div>
+                <div className="close" onClick={this.__onClickClose.bind(this)}></div>
             </div>
         );
     }
